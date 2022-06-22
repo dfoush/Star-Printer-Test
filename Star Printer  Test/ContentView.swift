986a3fe1660e408b3a17258c1eec60b1a4865b62
Dawson Foushee <Dawson.Foushee@accesscfa.com>
2022-06-22 08:55:25 -0400

    Initial Commit

---
+++ Star Printer  Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Star Printer  Test
+//
+//  Created by Foushee, Dawson on 6/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
